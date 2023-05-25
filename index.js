46c4fd089758e370870930e703a86be8f85d352b
lehung2022 <hung.le.programmer@gmail.com>
2023-05-25 19:56:32 +0700

    exercise three is done

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function printPage() {
-    window.print()
-  }
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+// This arrangement can be altered based on how we want the date's format to appear.
+let currentDate = `${day}-${month}-${year}`;
+console.log(currentDate); 
