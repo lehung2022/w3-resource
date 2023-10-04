ead0124e048fc95892b93aa07ce7bb61c0994afb
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 13:28:19 +0700

    exercise 14 is done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let var_name = "newVariable";
-let var_value = "newValue";
-  
-this[var_name] = var_value;
-console.log(var_name);
-console.log(this[var_name]);
-// write this one the screen
-// document.write(var_name);
-document.write(this[var_name]);
+var myDocument = "CV - Lê Hưng.docx";
+getFileExtentsion(myDocument)
 
+function getFileExtentsion () {
+    return myDocument.split('.').pop();
+}
+// write this one the screen
+document.write(myDocument);
+console.log(myDocument.split('.').pop());
