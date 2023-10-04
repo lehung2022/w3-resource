5cf28edaa757b5b81aa3c6110ad0a469ca0bb6f6
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 23:48:56 +0700

    exercise 19 is done

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-function comparison(a, b) {
-    if ((a + b) === 50) {
-        return 50;
-    }
-    else if ((a + b ) != 50) {
-        return a - 19
-    }
-    else if (a === 50 || b === 50) {
-        return 50;
-    } else {
-        return "We have a problem here"
-    }
+function testhundred(x) {
+    return ((Math.abs(100 - x) <= 20) ||
+        (Math.abs(400 - x) <= 20));
 }
-// The above code is my solution
 
-function test50(x, y) 
-{
-  return ((x == 50 || y == 50) || (x + y == 50));
-}
-console.log(test50(50, 50))
-console.log(test50(20, 50))
-console.log(test50(20, 20))
-console.log(test50(20, 30))
-// This one is from w3Resource
+console.log(testhundred(10));
+console.log(testhundred(90));
+console.log(testhundred(99));
+console.log(testhundred(199));
+console.log(testhundred(200));
 
-// write this one the screen
-document.write(comparison(20),);
+document.write(testhundred(40));
 
-// Display in the console
-console.log(comparison(10, 20));
-console.log(comparison(20, 30));
