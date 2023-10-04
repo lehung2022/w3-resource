ba75cefbdd6e45da888edcfca0080e193b88df93
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 00:00:27 +0700

    exercise 10 is done

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// check if the number is positive, negative or zero
-const number = prompt("Enter a number: ");
-
-if (number >= 0) {
-    if (number == 0) {
-        console.log("The number is zero");
-    } else {
-        console.log("The number is positive");
+function string_check(str1){
+    if (str1 === null || str1 === undefined || str1.substring(0, 2) === 'Py' ){
+return str1;
     }
-} else {
-    console.log("The number is negative");
+    return "Py"+str1;
 }
-
+console.log(string_check("Python"));
+console.log(string_check("thon"));
