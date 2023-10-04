df3798cc7be7e3a3cfddbb838e143141b6998e28
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 23:43:41 +0700

    exercise 18 is done

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-function sumTriple(a) {
-    if (a != 19) {
-        return a * 19;
+function comparison(a, b) {
+    if ((a + b) === 50) {
+        return 50;
     }
-    else if (a > 19) {
+    else if ((a + b ) != 50) {
         return a - 19
     }
-    else {
-        return a + 19;
+    else if (a === 50 || b === 50) {
+        return 50;
+    } else {
+        return "We have a problem here"
     }
 }
-// The above function calculated it wrong
+// The above code is my solution
 
-function diff_num(n) {
-    if (n <= 19) {
-      return (19 - n);
-      }
-    else
-      {
-       return (n - 19) * 3;
-      }
-  }
-// This is the correct one   
+function test50(x, y) 
+{
+  return ((x == 50 || y == 50) || (x + y == 50));
+}
+console.log(test50(50, 50))
+console.log(test50(20, 50))
+console.log(test50(20, 20))
+console.log(test50(20, 30))
+// This one is from w3Resource
 
 // write this one the screen
-document.write(diff_num(20),);
+document.write(comparison(20),);
 
 // Display in the console
-console.log(diff_num(10));
-console.log(diff_num(20));
+console.log(comparison(10, 20));
+console.log(comparison(20, 30));
