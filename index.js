5bdd8ba0814fe53d6818a7e57fe24e72a85ff31f
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 00:34:11 +0700

    exercise 30 is done. It is over. We have reached the maximum questions on one day. Continue in the afternoon of next day

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function check_three_nums(x, y, z) {
-    return (x >= 50 && x <= 99) || (y >= 50 && y <= 99) || (z >= 50 && z <= 99);
+function check_script(str) {
+    if (str.length < 6) {
+        return str;
+    }
+    let result_str = str;
+
+    if (str.substring(10, 4) == 'Script') {
+
+        result_str = str.substring(0, 4) + str.substring(10, str.length);
+
+    }
+    return result_str;
 }
 
-console.log(check_three_nums(50, 90, 99));
-console.log(check_three_nums(5, 9, 199));
-console.log(check_three_nums(65, 89, 199));
-console.log(check_three_nums(65, 9, 199));
+console.log(check_script("JavaScript"));
+console.log(check_script("CoffeeScript"));
