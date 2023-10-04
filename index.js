06d520db74c7819eca44ad0be923b8e5c8bc6ee0
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 11:08:53 +0700

    exercise 10 is done

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-let today = new Date();
-let christmasYear = today.getFullYear();
-
-if (today.getMonth() == 11 && today.getDate() > 25) {
-  christmasYear = christmasYear + 1;
+function multiplyBy() {
+  num1 = document.getElementById("fn").value;
+  num2 = document.getElementById("sn").value;
+  document.getElementById("result").innerHTML = num1 * num2;
 }
 
-let christmasDate = new Date(christmasYear, 11, 25);
-let dayMilliseconds = 1000 * 60 * 60 * 24;
-
-let remainingDays = Math.ceil(
-  (christmasDate.getTime() - today.getTime()) /
-  (dayMilliseconds)
-);
-
-// Write it to the page
-document.write("There are " + remainingDays +
-  " days remaining until Christmas.");
-
-console.log(today);
-console.log(christmasYear);
-console.log(christmasDate);
-console.log(dayMilliseconds);
-console.log(remainingDays);
+function divideBy() {
+  num1 = document.getElementById("fn").value;
+  num2 = document.getElementById("sn").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+} 
