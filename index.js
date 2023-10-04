df4dcbcf8a030bf8475e8bbc7fa567e5bfd59488
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 22:28:55 +0700

    exercise 15 is done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var myDocument = "CV - Lê Hưng.docx";
-getFileExtentsion(myDocument)
-
-function getFileExtentsion () {
-    return myDocument.split('.').pop();
+function difference(n) {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
 }
+
 // write this one the screen
-document.write(myDocument);
-console.log(myDocument.split('.').pop());
+document.write(difference(20), "\n");
+document.write(difference(32), "\n");
+document.write(difference(11), "\n");
+// Display in the console
+console.log(difference(20));
+console.log(difference(32));
+console.log(difference(11)); 
