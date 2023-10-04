4342c5779a975e0b4be9366f7e73f2cc76388d80
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 13:11:57 +0700

    exercise 13 is done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function GetURL() {
-    var gfg = document.URL;
-    document.getElementById("url").innerHTML = gfg;
-}
+let var_name = "newVariable";
+let var_value = "newValue";
+  
+this[var_name] = var_value;
+console.log(var_name);
+console.log(this[var_name]);
 // write this one the screen
-// document.write(url2);
+// document.write(var_name);
+document.write(this[var_name]);
 
