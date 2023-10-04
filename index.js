5e10f26bfe6b1fc32c62bfeeba008aff72efe4f8
lehung2022 <hung.le.programmer@gmail.com>
2023-10-04 22:32:49 +0700

    exercise 16 is done

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ function difference(n) {
 }
 
 // write this one the screen
-document.write(difference(20), "\n");
-document.write(difference(32), "\n");
-document.write(difference(11), "\n");
+document.write(difference(20),);
+
 // Display in the console
-console.log(difference(20));
-console.log(difference(32));
-console.log(difference(11)); 
+console.log(difference(32))
+console.log(difference(11))
