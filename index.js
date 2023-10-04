1df17ad9afc60597907a2d3162a9a6ec7272797f
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 00:32:18 +0700

    exercise 29 is done

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-function check_numbers(x, y) {
-    if ((x >= 50 && x <= 99) || (y >= 50 && y <= 99)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+function check_three_nums(x, y, z) {
+    return (x >= 50 && x <= 99) || (y >= 50 && y <= 99) || (z >= 50 && z <= 99);
 }
 
-console.log(check_numbers(12, 101));
-console.log(check_numbers(52, 80));
-console.log(check_numbers(15, 99));
+console.log(check_three_nums(50, 90, 99));
+console.log(check_three_nums(5, 9, 199));
+console.log(check_three_nums(65, 89, 199));
+console.log(check_three_nums(65, 9, 199));
