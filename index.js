e1d7b5ee0095782830737ad73e2848e575e0caf8
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 20:08:11 +0700

    exercise 44 is done

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function same_last_digit(p, q, r) {
-    return (p % 10 === q % 10) ||
-           (p % 10 === r % 10) ||
-           (q % 10 === r % 10);
-           
+function lessby20_others(x, y, z) {
+    return (x >= 20 && (x < y || x < z)) ||(y >= 20 && (y < x || y < z)) ||(z >= 20 && (z < y || z < x));
 }
-
-console.log(same_last_digit(22,32,42));
-console.log(same_last_digit(102,302,2));
-console.log(same_last_digit(20,22,45));
+console.log(lessby20_others(23, 45, 10));
+console.log(lessby20_others(23, 23, 10));
+console.log(lessby20_others(21, 66, 75));
