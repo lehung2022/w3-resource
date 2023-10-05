fe9e4fa05b08808833b6155f2d3db9594615ade8
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 20:37:51 +0700

    exercise 50 is done

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-function LetterChanges(str) {
-    var result = "";
-    for (var i = 0; i < str.length; i++) {
-        // handle "z"
-        if (122 == str.charCodeAt(i)) {
-            result += "a";
-        // handle "Z"
-        } else if (90 == str.charCodeAt(i)) {
-            result += "A";
-        // handle all other letter characters
-        } else if ((65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 89) ||
-                   (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 121)) {
-            result += String.fromCharCode(str.charCodeAt(i) + 1);
-        // append all other characters unchanged
-        } else {
-            result += str.charAt(i);
-        }
+function capital_letter(str) {
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
     }
-    return result;
+
+    return str.join(" ");
 }
 
-console.log(LetterChanges("AaZz+cod!foo")); // output: BbAa+dpe!gpp
+console.log(capital_letter("Write a JavaScript program to capitalize the first letter of each word of a given string."));
