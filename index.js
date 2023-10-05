e53b0e1a66fed5cd57ced48547327be41e17eef9
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 20:36:08 +0700

    exercise 49 is done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function string_reverse(str) {
-    return str.split("").reverse().join("");
+function LetterChanges(str) {
+    var result = "";
+    for (var i = 0; i < str.length; i++) {
+        // handle "z"
+        if (122 == str.charCodeAt(i)) {
+            result += "a";
+        // handle "Z"
+        } else if (90 == str.charCodeAt(i)) {
+            result += "A";
+        // handle all other letter characters
+        } else if ((65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 89) ||
+                   (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 121)) {
+            result += String.fromCharCode(str.charCodeAt(i) + 1);
+        // append all other characters unchanged
+        } else {
+            result += str.charAt(i);
+        }
+    }
+    return result;
 }
 
-console.log(string_reverse("I am now become death, the destroyer of worlds"));
-console.log(string_reverse("world wide web, IP address"));
-console.log(string_reverse("NextJS is more convenient than ReactJS, because it is a Framework"));
+console.log(LetterChanges("AaZz+cod!foo")); // output: BbAa+dpe!gpp
