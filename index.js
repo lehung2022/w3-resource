d7199d88c631429d4bc0d7ae81704acaf8d2975b
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 17:49:26 +0700

    exercise 37 is done

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function last_digit(x, y, z)
-{
-  if ((x > 0) && y > 0 && z > 0)
-    {
-     return (x % 10 == y % 10 && y % 10 == z % 10 && x % 10 == z % 10);
-   }
-  else
-    return false;
-}
+const str = 'Coding Beauty';
 
-console.log(last_digit(20, 30, 400));
-console.log(last_digit(-20, 30, 400));
-console.log(last_digit(20, -30, 400));
-console.log(last_digit(20, 30, -400));
+const first2 = str.slice(0, 2);
+console.log(first2); // Co
+
+const first6 = str.slice(0, 6);
+console.log(first6); // Coding
+
+const first8 = str.slice(0, 8);
+console.log(first8); // Coding B
