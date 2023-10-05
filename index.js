90f0f7773660c431e819df01012a1b12e4bfb71a
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 20:24:59 +0700

    exercise 48 is done

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-function test_digit(x, y, n) 
-  {    
-    if (n < 40 || n > 10000)
-      return false;
-    else
-      if (n >= x && n <= y)
-        return true;
-      else
-        return false;
-  }
-console.log(test_digit(40, 4000, 45));  
-console.log(test_digit(80, 320, 79));  
-console.log(test_digit(89, 4000, 30));
+function string_reverse(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("I am now become death, the destroyer of worlds"));
+console.log(string_reverse("world wide web, IP address"));
+console.log(string_reverse("NextJS is more convenient than ReactJS, because it is a Framework"));
