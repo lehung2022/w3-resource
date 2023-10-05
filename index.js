779ec3e7cda946af5ba3dbdcd8fb1ee6223818e6
lehung2022 <hung.le.programmer@gmail.com>
2023-10-05 17:24:27 +0700

    exercise 34 is completed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function ranges(x, y) {
-    if ((x >= 40 && x <= 60 && y >= 40 && y <= 60) 
-        || 
-        (x >= 70 && x <= 100 && y >= 70 && y <= 100))
-       {
-      return true;
-       } 
-      else 
-       {
-      return false;
+function max_townums_range(x, y){	
+    if( (x >= 40) && (x <= 60) && (y >= 40 && y <= 60) ){
+    if(x === y){
+    return "Numbers are the same";
+    }else if (x > y){
+    return x;
+    }else{
+    return y;
     }
-  }
-  
-  console.log(ranges(44, 56));
-  console.log(ranges(70, 95));
-  console.log(ranges(50, 89));
+    }else{
+    return "Numbers don't fit in range";
+    }
+    }
+    
+    console.log(max_townums_range(45, 60));
+    console.log(max_townums_range(25, 60));
+    console.log(max_townums_range(45, 80));
