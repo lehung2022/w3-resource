472e2dad1e3e37587f927ef345dd6d9e0dfc7407
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 13:28:50 +0700

    exercise 68 is done

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-function nop(str) {
-    let start_pos = 0;
-    let end_pos = str.length;
-
-    if (str.length > 0 && str.charAt(0) == 'P') {
-        start_pos = 1;
-    }
-
-    if (str.length > 1 && str.charAt(str.length - 1) == 'P') {
-        end_pos--;
-    }
-
-    return str.substring(start_pos, end_pos);
+function two_string(str, n) {
+    first_part = str.substring(0, n);
+    last_part = str.substring(str.length - n);
+    return first_part + last_part;
 }
 
-console.log(nop("PythonP"));
-console.log(nop("Python"));
-console.log(nop("JavaScript"));
+console.log(two_string("JavaScript", 2));
+console.log(two_string("JavaScript", 3));
