d7fdfbdbb64fc69103eb499e42fd5eec19d9aa19
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 13:13:44 +0700

    exericse 63 is done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+function middle_three(str) {
+    if (str.length % 2 != 0) {
 
-function right_three(str) {
-    if (str.length > 1)
-      {
-        return str.slice(-3) + str.slice(0, -3);
-      }
- return str;
+        mid = (str.length + 1) / 2;
+        return str.slice(mid - 2, mid + 1);
+    }
+    return str;
 }
-console.log(right_three("Python"));
-console.log(right_three("JavaScript"));
-console.log(right_three("Hi"));
+console.log(middle_three('abcdefg'));
+console.log(middle_three('HTML5'));
+console.log(middle_three('Python'));
+console.log(middle_three('PHP'));
+console.log(middle_three('Exercises'));
