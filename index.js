6ebb586f14f2c29fe06285ba7fc56e93cf114947
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 23:52:04 +0700

    fixed exercise 05

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-// function for left rotation
-function left_rotation(str,k){
-
-    // getting prefix elements and remaining elements
-    // switiching there place
-    var new_str = str.substr(k) + str.substring(0,k);
- 
-    // printing the rotated string
-    console.log("String after kth left rotation is: " + new_str);
- }
- 
- // function for right rotation
- function right_rotation(str,k){
- 
-    // getting suffix elements and remaining elements
-    // switiching there place
- 
-    var new_str = str.substr(str.length - k) + str.substring(0,str.length-k);
-    // printing the rotated string
- 
-    console.log("String after kth rigth rotation is: " + new_str);
- }
- 
- // given string
- var str = "apple"
- var k = 2
- 
- // printing the given string
- console.log("The given string is: " + str);
- 
- // getting left rotation
- 
- left_rotation(str,k);
- 
- // getting right rotation
- right_rotation(str,k)
+const rotate = (text, n) => {
+    txtArr = text.split("")
+  
+    if (n > 0) {
+      for (let i = 1; i <= n; i++) {
+        let firstel = txtArr[0]
+        txtArr.shift()
+        txtArr.push(firstel)
+      }
+    }
+    else if (n < 0) {
+      for (let i = -1; i >= n; i--) {
+        let lastel = txtArr[txtArr.length - 1]
+        txtArr.pop()
+        txtArr.unshift(lastel)
+      }
+    }
+  
+    console.log(txtArr.join(""))
+  }
+  
+  rotate("hello", -2)
+  
