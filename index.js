fea7100d51912ee2e3c51f486eedf47eb8f87dfa
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 22:28:39 +0700

    exercise 79 is done

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function is13(nums) {
-    if (nums.indexOf(1) == -1 && nums.indexOf(3) == -1) {
-        return true;
-    } else {
-        return false;
-    }
+function twice3040(arra1) {
+    let a = arra1[0],
+        b = arra1[1];
+    return (a === 30 && b === 30) || (a === 40 && b === 40);
 }
 
-console.log(is13([7, 8]));
-console.log(is13([3, 2]));
-console.log(is13([0, 1]));
+console.log(twice3040([30, 30]));
+console.log(twice3040([40, 40]));
+console.log(twice3040([20, 20]));
+console.log(twice3040([30]));
