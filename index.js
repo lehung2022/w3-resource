1de1dee9aa629976f87cc47015ca114a2266a9ea
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 22:26:56 +0700

    exercise 78 is done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function contins13(nums) {
-
-    if (nums.indexOf(1) != -1 || nums.indexOf(3) != -1) {
-        return true
-    }
-    else {
-        return false
+function is13(nums) {
+    if (nums.indexOf(1) == -1 && nums.indexOf(3) == -1) {
+        return true;
+    } else {
+        return false;
     }
 }
+
+console.log(is13([7, 8]));
+console.log(is13([3, 2]));
+console.log(is13([0, 1]));
