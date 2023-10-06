1511a753656caee21e8d85049733795500f6f24e
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 13:07:07 +0700

    exercise 61 is done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function without_first_end(str) {
-    return str.substring(1, str.length - 1);
+function concatenate(str1, str2) {
+    str1 = str1.substring(1, str1.length);
+    str2 = str2.substring(1, str2.length);
+    return str1 + str2;
 }
-console.log(without_first_end('JavaScript'));
-console.log(without_first_end('JS'));
-console.log(without_first_end('PHP'));
+
+console.log(concatenate("PHP", "JS"));
+console.log(concatenate("A", "B"));
+console.log(concatenate("AA", "BB"));
