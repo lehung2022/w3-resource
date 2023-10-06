ac1559c0603414fbc1865a7f9a926a09ed5a56ce
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 23:36:22 +0700

    fixed exercise 05

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-function animate_string(id) 
-{
-    var element = document.getElementById(id);
-    var textNode = element.childNodes[0]; // assuming no other children
-    var text = textNode.data;
+// function for left rotation
+function left_rotation(str,k){
 
-setInterval(function () 
-{
- text = text[text.length - 1] + text.substring(0, text.length - 1);
-  textNode.data = text;
-}, 100);
-}
+    // getting prefix elements and remaining elements
+    // switiching there place
+    var new_str = str.substr(k) + str.substring(0,k);
+ 
+    // printing the rotated string
+    console.log("String after kth left rotation is: " + new_str);
+ }
+ 
+ // function for right rotation
+ function right_rotation(str,k){
+ 
+    // getting suffix elements and remaining elements
+    // switiching there place
+ 
+    var new_str = str.substr(str.length - k) + str.substring(0,str.length-k);
+    // printing the rotated string
+ 
+    console.log("String after kth rigth rotation is: " + new_str);
+ }
+ 
+ // given string
+ var str = "apple"
+ var k = 2
+ 
+ // printing the given string
+ console.log("The given string is: " + str);
+ 
+ // getting left rotation
+ 
+ left_rotation(str,k);
+ 
+ // getting right rotation
+ right_rotation(str,k)
