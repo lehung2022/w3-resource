eed8d8480656c2abd05fa2b7aa13c1a330798530
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 22:50:37 +0700

    exercise 84 is done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var plorp = ["bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
-var longest = plorp.reduce(function (a, b) {
-    return a.length > b.length ? a : b
-}, '');
-console.log(longest);
+function alphabet_char_Shift(str) {
+    var all_chars = str.split("");
+    for (var i = 0; i < all_chars.length; i++) {
+        var n = all_chars[i].charCodeAt() - 'a'.charCodeAt();
+        n = (n + 1) % 26;
+        all_chars[i] = String.fromCharCode(n + 'a'.charCodeAt());
+    }
+    return all_chars.join("");
+}
+
+console.log(alphabet_char_Shift("abcdxyz"))
