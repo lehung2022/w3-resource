ffa65550ab93bab36bb0a26ca7ce82aebd419d65
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 13:11:14 +0700

    exercise-62 is done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function concatenate(str1, str2) {
-    str1 = str1.substring(1, str1.length);
-    str2 = str2.substring(1, str2.length);
-    return str1 + str2;
-}
 
-console.log(concatenate("PHP", "JS"));
-console.log(concatenate("A", "B"));
-console.log(concatenate("AA", "BB"));
+function right_three(str) {
+    if (str.length > 1)
+      {
+        return str.slice(-3) + str.slice(0, -3);
+      }
+ return str;
+}
+console.log(right_three("Python"));
+console.log(right_three("JavaScript"));
+console.log(right_three("Hi"));
