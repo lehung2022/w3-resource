ea552338e1d31e162a83c24d2fa77a9c313e51bf
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 22:25:16 +0700

    exercise 77 is done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function started(nums) {
-    var array1 = [];
-    array1.push(nums[0], nums[nums.length - 1]);
+function contins13(nums) {
 
-    return array1;
+    if (nums.indexOf(1) != -1 || nums.indexOf(3) != -1) {
+        return true
+    }
+    else {
+        return false
+    }
 }
-console.log(started([20, 20, 30]));
-console.log(started([5, 2, 7, 8]));
-console.log(started([17, 12, 34, 78]));
