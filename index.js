a805b6a63aa72c19096874b34618fa5279157130
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 22:40:05 +0700

    exercise 81 is done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function swap(arrayA) {
-    [arrayA[0], array[arrayA.length - 1]] = [arrayA[arrayA.length - 1], arrayA[0]];
-    return arrayA;
-}
-console.log(swap([1, 2, 3, 4]));
-console.log(swap([0, 2, 1]));
-console.log(swap([3]));
+function add_two_digits(n)
+   {
+      return n % 10 + Math.floor(n / 10);
+   }
+console.log(add_two_digits(25))
+console.log(add_two_digits(50))
