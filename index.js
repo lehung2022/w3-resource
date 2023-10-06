6cfdef0cc32bcd06133b4238000a5d3d81535519
lehung2022 <hung.le.programmer@gmail.com>
2023-10-06 13:18:14 +0700

    exercise 65 is done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-function str_con_cat(str1, str2) {
-    const m = Math.min(str1.length, str2.length);
-
-    return str1.substring(str1.length - m) + str2.substring(str2.length - m);
+function end_script(str) {
+    if (str.substring(str.length - 6, str.length) == 'Script') {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-
-console.log(str_con_cat("Python", "JS"));
-console.log(str_con_cat("ab", "cdef"));
+console.log(end_script("JavaScript"));
+console.log(end_script("Java Script"));
+console.log(end_script("Java Scripts"));
