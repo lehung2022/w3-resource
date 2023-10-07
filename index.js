dce1562e038578f97e39375c632c066658dc6d85
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 17:18:23 +0700

    exercise 92 needs to be pulled, fetched and merged

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-function array_max_consecutive_sum(nums, k) {
-    let result = 0;
-    let temp_sum = 0;
-    for (var i = 0; i < k - 1; i++) {
-        temp_sum += nums[i];
-    }
-    for (var i = k - 1; i < nums.length; i++) {
-        temp_sum += nums[i];
-        if (temp_sum > result) {
-            result = temp_sum;
-        }
-        temp_sum -= nums[i - k + 1];
-    }
-    return result;
+function max_difference(arr) {
+	var max = -1;
+    var temp;
+	for (var i = 0; i < arr.length - 1; i++)
+      {
+		temp = Math.abs(arr[i] - arr[i + 1]);
+		max = Math.max(max, temp);
+	  }
+	return max;
 }
 
-console.log(array_max_consecutive_sum([1, 2, 3, 14, 5], 2))
-console.log(array_max_consecutive_sum([2, 3, 5, 1, 6], 3))
-console.log(array_max_consecutive_sum([9, 3, 5, 1, 7], 2))
+console.log(max_difference([1, 2, 3, 8, 9]))
+console.log(max_difference([1, 2, 3, 18, 9]))
+console.log(max_difference([13, 2, 3, 8, 9]))
