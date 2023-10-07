ac6a4db2be0cd230af38d1d5f6923c7c3d4bd9d3
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 19:48:49 +0700

    there are still a bunch of conflicts I need to solve

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function sum_adjacent_difference(arr) {
-    var result = 0;
-    for (var i = 1; i < arr.length; i++) {
-        result += Math.abs(arr[i] - arr[i - 1]);
-    }
-    return result;
-}
+function array_element_mode(arr) {
+    var ctr = [],
+        ans = 0;
 
-console.log(sum_adjacent_difference([1, 2, 3, 2, -5]));
+    for (var i = 0; i < 10; i++) {
+        ctr.push(0);
+    }
+    for (var i = 0; i < arr.length; i++) {
+        ctr[arr[i] - 1]++;
+        if (ctr[arr[i] - 1] > ctr[ans]) {
+            ans = arr[i] - 1;
+        }
+    }
+    return ans + 1;
+}
+console.log(array_element_mode([1, 2, 3, 2, 2, 8, 1, 9]))
