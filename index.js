3f48c1cd37b889eea64183199ec8807a91e7d201
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 17:13:20 +0700

    done

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const rotate = (text, n) => {
-    txtArr = text.split("")
-  
-    if (n > 0) {
-      for (let i = 1; i <= n; i++) {
-        let firstel = txtArr[0]
-        txtArr.shift()
-        txtArr.push(firstel)
-      }
+function array_max_consecutive_sum(nums, k) {
+    let result = 0;
+    let temp_sum = 0;
+    for (var i = 0; i < k - 1; i++) {
+        temp_sum += nums[i];
     }
-    else if (n < 0) {
-      for (let i = -1; i >= n; i--) {
-        let lastel = txtArr[txtArr.length - 1]
-        txtArr.pop()
-        txtArr.unshift(lastel)
-      }
+    for (var i = k - 1; i < nums.length; i++) {
+        temp_sum += nums[i];
+        if (temp_sum > result) {
+            result = temp_sum;
+        }
+        temp_sum -= nums[i - k + 1];
     }
-  
-    console.log(txtArr.join(""))
-  }
-  
-  rotate("hello", -2)
-  
+    return result;
+}
+
+console.log(array_max_consecutive_sum([1, 2, 3, 14, 5], 2))
+console.log(array_max_consecutive_sum([2, 3, 5, 1, 6], 3))
+console.log(array_max_consecutive_sum([9, 3, 5, 1, 7], 2))
