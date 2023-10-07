affe9ea0566665e55a5f753125cee5c429ca2b70
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 17:27:39 +0700

    fixed the style file as well

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function max_difference(arr) {
-	var max = -1;
-    var temp;
-	for (var i = 0; i < arr.length - 1; i++)
-      {
-		temp = Math.abs(arr[i] - arr[i + 1]);
-		max = Math.max(max, temp);
-	  }
-	return max;
-}
+function array_max_diff(arr) {
 
-console.log(max_difference([1, 2, 3, 8, 9]))
-console.log(max_difference([1, 2, 3, 18, 9]))
-console.log(max_difference([13, 2, 3, 8, 9]))
+    var max_result = 0;
+
+    for(var i=0;i<arr.length;i++)
+       {
+        for(var k=0; k!=i && k<arr.length; k++)
+        {
+            var diff = Math.abs(arr[i]-arr[k]);
+            max_result = Math.max(max_result, diff);
+        }
+    }
+    return max_result;
+}
+console.log(array_max_diff([1, 2, 3, 8, 9]))
+console.log(array_max_diff([1, 2, 3, 18, 9]))
+console.log(array_max_diff([13, 2, 3, 8, 9]))
