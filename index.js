f8ef76f931cc78e8de4d2dfae3657b397a30a260
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 19:14:57 +0700

    what the fuck

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function change_case(new_str) {
-    var x = 0;
-    var y = 0;
+function array_element_mode(arr) {
+    var ctr = [],
+        ans = 0;
 
-    for (var i = 0; i < new_str.length; i++) {
-        if (/[A-Z]/.test(new_str[i])) {
-            x++;
-        } else y++;
+    for (var i = 0; i < 10; i++) {
+        ctr.push(0);
     }
-
-    if (y > x) return new_str.toLowerCase();
-    return new_str.toUpperCase();
+    for (var i = 0; i < arr.length; i++) {
+        ctr[arr[i] - 1]++;
+        if (ctr[arr[i] - 1] > ctr[ans]) {
+            ans = arr[i] - 1;
+        }
+    }
+    return ans + 1;
 }
-
-console.log(change_case("Write"))
-console.log(change_case("PHp"))
+console.log(array_element_mode([1, 2, 3, 2, 2, 8, 1, 9]))
