68e96ff37a7cc1ebb74d2b956b23dff06eb8fca3
lehung2022 <hung.le.programmer@gmail.com>
2023-10-07 18:57:41 +0700

    exercise 96

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function array_element_replace(arr, old_value, new_value) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === old_value) {
-            arr[i] = new_value;
-        }
+function sum_adjacent_difference(arr) {
+    var result = 0;
+    for (var i = 1; i < arr.length; i++) {
+        result += Math.abs(arr[i] - arr[i - 1]);
     }
-    return arr;
+    return result;
 }
-num = [1, 2, 3, 2, 2, 8, 1, 9];
-console.log("Original Array: " + num);
-console.log(array_element_replace(num, 2, 5));
+
+console.log(sum_adjacent_difference([1, 2, 3, 2, -5]));
