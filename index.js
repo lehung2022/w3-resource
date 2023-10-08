7df6b71cfd5cef4ed7e9c03081bf26e8a67d6157
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:27:09 +0700

    meh

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function number_of_InversionsNaive(arr) {
-    var ctr = 0;
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) 
-              ctr++;
-        }
-    }
-    return ctr;
+// program to check leap year
+function checkLeapYear(year) {
+
+  //three conditions to find out the leap year
+  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+      console.log(year + ' is a leap year');
+  } else {
+      console.log(year + ' is not a leap year');
+  }
 }
 
-console.log(number_of_InversionsNaive([0, 3, 2, 5, 9]));   
-console.log(number_of_InversionsNaive([1, 5, 4, 3]));   
-console.log(number_of_InversionsNaive([10, 30, 20, -10]));  
+// take input
+const year = prompt('Enter a year:');
+
+checkLeapYear(year);
