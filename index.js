f63aa4a99dc09be38d332b9cc98c0290d1902da5
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:17:16 +0700

    exercise 06

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// program to check leap year
-function checkLeapYear(year) {
-
-  //three conditions to find out the leap year
-  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-      console.log(year + ' is a leap year');
-  } else {
-      console.log(year + ' is not a leap year');
-  }
+function leapyear(year)
+{
+return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 }
-
-// take input
-const year = prompt('Enter a year:');
-
-checkLeapYear(year);
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
