18bbc0b650296aa5361d210e6508fca045d214e6
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:21:25 +0700

    exercise 07

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
-// solution 1
-// let year = 2014;
-// while(year <= 2050) {
-//   if(new Date(year, 0, 1).getDay() === 0) {
-//     break;
-//   }
-//   year++;
-// }
-
-// console.log(year);
-
-// solution 2
-let s = `Enter the value of Year1
-and Year2 in the input box to get
-year's having Sunday on 1st January`;
-
-document.getElementById("geeks")
-.innerHTML = `<p>${s}</p>`;
-
-function GFG_Fun() {
-var y1 = Number(document
-.getElementById('year1').value);
-
-var y2 = Number(document
-.getElementById('year2').value);
-
-var res = " ";
-
-for (let year = y1; year <= y2; ++year) {
-	const c_year=new Date(year, 0, 1);
-	if (c_year.getDay()===0) {
-	res +=year + " ," ;
-	}
-}
-	if (y1> y2) {
-	document.getElementById('gfg').innerHTML
-	= "Year2 must be greater than Year1";
-	} else {
-	if (res === " ") {
-	document.getElementById('gfg')
-	.innerHTML = `<p>No Year Exist</p>`;
-	} else {
-	document.getElementById('gfg')
-	.innerHTML = `<p> Year having
-		Sunday on 1st January are :${res}</p>`;
-	}
-	}
-	}
+console.log('--------------------');
+for (var year = 2014; year <= 2050; year++)
+    {
+    var d = new Date(year, 0, 1);
+    if ( d.getDay() === 0 )
+        console.log("1st January is being a Sunday  "+year);
+    }
+console.log('--------------------');
