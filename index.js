202b93cfea9b36032201f4a59d1de6a4cc9a2fce
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:14:25 +0700

    exercise 05 is done

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const rotate = (text, n) => {
-    txtArr = text.split("")
-  
-    if (n > 0) {
-      for (let i = 1; i <= n; i++) {
-        let firstel = txtArr[0]
-        txtArr.shift()
-        txtArr.push(firstel)
-      }
-    }
-    else if (n < 0) {
-      for (let i = -1; i >= n; i--) {
-        let lastel = txtArr[txtArr.length - 1]
-        txtArr.pop()
-        txtArr.unshift(lastel)
-      }
-    }
-  
-    console.log(txtArr.join(""))
-  }
-  
-  rotate("hello", -2)
-  
+function animate_string(id) 
+{
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function () 
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
