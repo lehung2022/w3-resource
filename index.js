d1b50c74b9318f63d7dbbf2529d02226309cfce3
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:40:17 +0700

    meh

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-console.log('--------------------');
-for (var year = 2014; year <= 2050; year++)
-    {
-    var d = new Date(year, 0, 1);
-    if ( d.getDay() === 0 )
-        console.log("1st January is being a Sunday  "+year);
-    }
-console.log('--------------------');
+function leapyear(year)
+{
+return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
