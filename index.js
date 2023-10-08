3fa03daddf75c52ee1697843f296ab28a976daba
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:37:52 +0700

    meh

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function number_of_InversionsNaive(arr) {
-    var ctr = 0;
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) 
-              ctr++;
-        }
-    }
-    return ctr;
+let today = new Date();
+let christmasYear = today.getFullYear();
+
+if (today.getMonth() == 11 && today.getDate() > 25) {
+  christmasYear = christmasYear + 1;
 }
 
-console.log(number_of_InversionsNaive([0, 3, 2, 5, 9]));   
-console.log(number_of_InversionsNaive([1, 5, 4, 3]));   
-console.log(number_of_InversionsNaive([10, 30, 20, -10]));  
+let christmasDate = new Date(christmasYear, 11, 25);
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+let remainingDays = Math.ceil(
+  (christmasDate.getTime() - today.getTime()) /
+  (dayMilliseconds)
+);
+
+// Write it to the page
+document.write("There are " + remainingDays +
+  " days remaining until Christmas.");
+
+console.log(today);
+console.log(christmasYear);
+console.log(christmasDate);
+console.log(dayMilliseconds);
+console.log(remainingDays);
+
