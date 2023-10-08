17884b513163f2c3e3dbca4ae1a11b3e0379e4fe
lehung2022 <hung.le.programmer@gmail.com>
2023-10-08 16:29:11 +0700

    exercise 08

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-// program to check leap year
-function checkLeapYear(year) {
+// solution 1
+// let year = 2014;
+// while(year <= 2050) {
+//   if(new Date(year, 0, 1).getDay() === 0) {
+//     break;
+//   }
+//   year++;
+// }
 
-  //three conditions to find out the leap year
-  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-      console.log(year + ' is a leap year');
-  } else {
-      console.log(year + ' is not a leap year');
-  }
+// console.log(year);
+
+// solution 2
+let s = `Enter the value of Year1
+and Year2 in the input box to get
+year's having Sunday on 1st January`;
+
+document.getElementById("geeks")
+.innerHTML = `<p>${s}</p>`;
+
+function GFG_Fun() {
+var y1 = Number(document
+.getElementById('year1').value);
+
+var y2 = Number(document
+.getElementById('year2').value);
+
+var res = " ";
+
+for (let year = y1; year <= y2; ++year) {
+	const c_year=new Date(year, 0, 1);
+	if (c_year.getDay()===0) {
+	res +=year + " ," ;
+	}
 }
-
-// take input
-const year = prompt('Enter a year:');
-
-checkLeapYear(year);
+	if (y1> y2) {
+	document.getElementById('gfg').innerHTML
+	= "Year2 must be greater than Year1";
+	} else {
+	if (res === " ") {
+	document.getElementById('gfg')
+	.innerHTML = `<p>No Year Exist</p>`;
+	} else {
+	document.getElementById('gfg')
+	.innerHTML = `<p> Year having
+		Sunday on 1st January are :${res}</p>`;
+	}
+	}
+	}
